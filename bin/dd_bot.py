37fffb5ad128270a868b4291f6f5f3296cb1ab88
Tom Nehil <tom.nehil@gmail.com>
2016-03-15 22:02:14 -0500

    Move feeds to separate json

--- bin/dd_bot.py
+++ bin/dd_bot.py
@@ -3,6 +3,7 @@
 import feedparser
 import re
 import os
+import json
 from slacker import Slacker
 
 slack_token = os.environ['DD_BOT_TOKEN']
@@ -12,22 +13,18 @@ slack = Slacker(slack_token)
 redis_url = os.environ['DD_REDIS_URL']
 cache = redis.StrictRedis.from_url(redis_url)
 
-feeds = [("MINNPOST", "https://www.minnpost.com/rss.xml"),
-         ("THE STAR TRIBUNE", "http://www.startribune.com/rss/?sf=1&s=/"),
-         ("THE STAR TRIBUNE", "http://www.startribune.com/local/index.rss2"),
-         ("THE STAR TRIBUNE", "http://www.startribune.com/politics/index.rss2"),
-         ("THE PIONEER PRESS","http://www.twincities.com/feed/"),
-         ("MPR","http://feeds.mpr.org/MPR_NewsFeatures"),
-        ]
+fp = open('../feeds.json', 'r')
+feeds = json.load(fp)['feeds']
+fp.close()
 
 exp = re.compile(r'[Dd]oubl.*\b[Dd]own')
 
 for feed in feeds:
-    title, url = feed
+    outlet, url = feed['outlet'], feed['feed_url']
     f = feedparser.parse(url)
     for entry in f['entries']:
         if re.search(exp, entry['title']) and not cache.get(entry['link']):
             slack.chat.post_message(slack_channel,
-                                    "%s IS DOUBLING DOWN: %s" % (title, entry['link']),
+                                    "%s IS DOUBLING DOWN: %s" % (outlet, entry['link']),
                                     as_user=True)
             cache.setex(entry['link'], 60*60*24*7, '')
