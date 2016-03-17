2e8768485dc716c1b4ee70a7ebb8d2327e278a2d
Tom Nehil <tom.nehil@gmail.com>
2016-03-16 20:13:02 -0500

    Changes feeds.json to config.json, adds count of double-downs

--- bin/dd_bot.py
+++ bin/dd_bot.py
@@ -7,24 +7,30 @@ import json
 from slacker import Slacker
 
 slack_token = os.environ['DD_BOT_TOKEN']
-slack_channel = "#socialmedia" #update to use env variable probably
 slack = Slacker(slack_token)
 
 redis_url = os.environ['DD_REDIS_URL']
 cache = redis.StrictRedis.from_url(redis_url)
 
-fp = open('../feeds.json', 'r')
-feeds = json.load(fp)['feeds']
+fp = open('../config.json', 'r')
+config = json.load(fp)
 fp.close()
+
+slack_channel = config['slack_channel']
+feeds = config['feeds']
+
 
 exp = re.compile(r'[Dd]oubl.*\b[Dd]own')
 
 for feed in feeds:
     outlet, url = feed['outlet'], feed['feed_url']
     f = feedparser.parse(url)
+    dds = 0
     for entry in f['entries']:
         if re.search(exp, entry['title']) and not cache.get(entry['link']):
             slack.chat.post_message(slack_channel,
                                     "%s IS DOUBLING DOWN: %s" % (outlet, entry['link']),
                                     as_user=True)
             cache.setex(entry['link'], 60*60*24*7, '')
+            dds +=1
+    print("Found %s double-downs" % dds)
